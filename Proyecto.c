e15ca4869ac316942034b980114834a5c4639e45
antorresca <65253336+antorresca@users.noreply.github.com>
2023-05-30 09:01:03 -0500

    Configuraciones

--- Proyecto.c
+++ Proyecto.c
@@ -7,13 +7,47 @@
  * Puerto B (RB0-RB7) para Teclado matricial
 */
 
+//Variables
+int contador = 0;
+int duty = 0;
+
 //Prototipos
 unsigned char LeerTeclado(void);
 
 void main(void){
     //Configuracion de pines
     TRISB = 0;
+    OSCCON = 0b11110110; //Frecuencia a 8MHz
+    //Configuracion del LCD
     TRISD = 0;
+    TRISE0 = 0;
+    TRISE1 = 0;
+    InicializaLCD();
+    BorraLCD();
+    //Fin de configuracion del LCD
+    //Configuracion del Timer 0
+    T0CON = 0b0000110;
+    TMR0 = 49911; //Precarga para 1 segundo
+    TMR0IF = 0;
+    TMR0IE = 1;
+    TMR0ON=1;
+    GIE = 1;
+    //Fin de configuracion del Timer 0
+    //Configuracion para PWM
+    TRISC2 = 0; //Canal de salida para PWM
+    PR2 = 249; 
+    CCPR1L = 0; //Para canal escogido ***PUEDE GENERAR PROBLEMAS***
+    T2CON=0b00000000;
+    CCP1CON=0b00001100;
+    TMR2=0;
+    TMR2ON=1;
+    //Fin de cofiguracion para PWM 
+    //Configuracion de comunicacion
+    TRISC6 = 1;
+    TRISC7 = 1;
+    TXSTA = 0b00100000;
+    RCSTA = 0b10010000;
+    //Fin de configuracion de comunicacion    
     while(1){
         
     }
@@ -51,4 +85,16 @@ unsigned char LeerTeclado(void){
     }
     }
     return ' ';
+}
+
+void __interrupt() ISR(void){
+    if(TMR0IF == 1){
+        TMR0IF=0;
+        TMR0 = 49911;
+        contador += 1;
+        if(contador == 20){
+            SLEEP();
+            while(1);
+        }
+    }
 }
